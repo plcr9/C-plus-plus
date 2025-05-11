ee6055d586af3bfe52bbf0704319907e92a09c3a
Paul <118978940+plcr9@users.noreply.github.com>
2025-05-11 22:21:08 +0100

    Update variables2.cpp

--- variables2.cpp
+++ variables2.cpp
@@ -1,1 +1,14 @@
+int main() {
 
+  double tempf;
+  double tempc;
+
+  std::cout << "Enter the temperature in Fahrenheit: ";
+
+  st::cin >> tempf;
+
+  tempc = (tempf - 32) / 1.8;
+
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+
+}
