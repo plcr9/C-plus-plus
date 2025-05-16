73c618fda24278b7c823e0b1027568c8b88404ed
Paul <118978940+plcr9@users.noreply.github.com>
2025-05-16 07:07:25 +0100

    Update coin_flip.cpp

--- coin_flip.cpp
+++ coin_flip.cpp
@@ -1,1 +1,17 @@
+int main() {
+  
+  srand (time(NULL));
+  int coin = rand() % 2;
 
+  if (coin == 0) {
+    
+    std::cout << "Heads\n";
+
+  }
+  else {
+    
+    std::cout << "Tails\n";
+
+  }
+
+}
