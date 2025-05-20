717eeec77902d45f492af6a21ea0e14f2bc3adf2
Paul <118978940+plcr9@users.noreply.github.com>
2025-05-20 09:46:52 +0100

    Update else_if.cpp

--- else_if.cpp
+++ else_if.cpp
@@ -1,1 +1,21 @@
+int main() {
 
+  double ph = 4.6;
+
+  if (ph > 7) {
+
+    std::cout << "Basic\n";
+
+  }
+  else if (ph < 7) {
+
+    std::cout << "Acidic\n";
+
+  }
+  else {
+
+    std::cout << "Neutral\n";
+
+  }
+
+}
