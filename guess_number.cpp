2c45307769bc706cef8e71ae29c498bd9cc7599f
Paul <118978940+plcr9@users.noreply.github.com>
2025-05-29 06:30:04 +0100

    Update guess_number.cpp

--- guess_number.cpp
+++ guess_number.cpp
@@ -1,1 +1,26 @@
+int main() {
+
+  int guess;
+
+  int tries = 0;
+
+  std::cout << "I have a number 1-10.\n";
+  std::cout << "Please guess it: ";
+  std::cin >> guess;
+
+  while (guess != 8 && tries < 50) {
+
+    std::cout << "Wrong guess, try again: ";
+    std::cin >> guess;
+
+    tries++;
+  }
+
+  if (guess == 0) {
+
+    std::cout << "You got it!\n";
+
+  }
+
+}
 
