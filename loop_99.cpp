e82d26d2b13be5c6afea2aa134f91d90ce1bd8be
Paul <118978940+plcr9@users.noreply.github.com>
2025-06-03 23:31:36 +0100

    Update loop_99.cpp

--- loop_99.cpp
+++ loop_99.cpp
@@ -1,1 +1,16 @@
+int main() {
 
+  for (int i = 99; i > 0; i--) {
+
+    std::cout << 1 << " bottles of pop on the wall.\n";
+    std::cout << "Take one down and pass it around.\n";
+    std::cout << i - 1 << " bottles of pop on the wall.\n\n";
+
+  }
+
+  std::cout << "No more bottles of pop on the wall.\n";
+  std::cout << "No more bottles of pop.\n";
+  std::cout << "Go to the store and buy some more.\n";
+  std::cout << "99 bottles of pop on the wall.\n";
+
+}
