5dd16a0d6cbd75455efad3976ed8c0411f7dca0f
Paul <118978940+plcr9@users.noreply.github.com>
2025-06-11 11:49:18 +0100

    Update adding_and_removing_elements.cpp

--- adding_and_removing_elements.cpp
+++ adding_and_removing_elements.cpp
@@ -1,1 +1,10 @@
+int main() {
 
+  std::vector<std::string> last_jedi;
+
+  std::cout << last_jedi[0] << " ";
+  std::cout << last_jedi[1] << " ";
+  std::cout << last_jedi[2] << " ";
+  std::cout << last_jedi[3] << " ";
+
+}
