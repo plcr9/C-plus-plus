8f1ccc7a98911e7e61b05a879822ba15bb973bda
Paul <118978940+plcr9@users.noreply.github.com>
2025-06-12 07:41:13 +0100

    Update vector_size.cpp

--- vector_size.cpp
+++ vector_size.cpp
@@ -1,1 +1,10 @@
+int main() {
 
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+
+  grocery.push_back("Artichoke");
+  grocery.push_back("Eggs");
+
+  std::cout << grocery.size() << "\n";
+
+}
