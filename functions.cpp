29fce23284ff8239ba90ad2f62b3aad93db07b42
Paul <118978940+plcr9@users.noreply.github.com>
2025-06-17 10:16:43 +0100

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,14 @@
+int main () {
 
+  std::string on_off_attempt;
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cout >> on_off_attempt;
+
+  std::cout << "Oh hi Jen!\n";
+
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+  std::cin >> on_off_attempt;
+
+}
