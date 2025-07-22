54dc9f11138802a170dfd163457a1ea4efc5da63
Paul <118978940+plcr9@users.noreply.github.com>
2025-07-22 06:50:09 +0100

    Update tackling_multiple_arguments.cpp

--- tackling_multiple_arguments.cpp
+++ tackling_multiple_arguments.cpp
@@ -1,1 +1,18 @@
+void name_x_time(std::string name, int x) {
 
+  while (x > 0) {
+
+    std::cout << name;
+    x--;
+
+  }
+
+}
+
+int main() {
+
+  std::string my_name = "Add your name here!";
+  int some_number = 5;
+  name_x_times(my_name, some_number);
+
+}
