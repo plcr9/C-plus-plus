5ce2c464f83da0147369bb4ac5da4af886e1abc5
Paul <118978940+plcr9@users.noreply.github.com>
2025-07-23 19:43:54 +0100

    Update cplusplus_function_review.cpp

--- cplusplus_function_review.cpp
+++ cplusplus_function_review.cpp
@@ -1,1 +1,26 @@
+std::string it_convo() {
+
+  std::cout << "Hello. IT.\n";
+  std::cout << "Have you tried turning it off and on again? y/n\n";
+
+  std::string reply;
+  std::cin >> reply;
+
+}
+
+int main() {
+
+  std::string on_off_attempt;
+  on_off_attempt = it_convo();
+
+  std::cout << "Oh it Jen!\n";
+
+  on_off_attempt = it_convo();
+
+  std::cout << "You stole the sun from my heart? Yes, that's stealing!\n";
+
+  on_off_attempt = it_convo();
+
+}
+
 
