fbc5025f2aa83766d698edadb4a72f0a76ca3cd9
Paul <118978940+plcr9@users.noreply.github.com>
2025-07-24 06:06:44 +0100

    Update cplusplus_function_intro.cpp

--- cplusplus_function_intro.cpp
+++ cplusplus_function_intro.cpp
@@ -1,1 +1,9 @@
+void introduction(std::string first_name, std::string_last_name) {
+  std::cout << last_name << ", " << first_name << " " << last_name << "\n";
+}
 
+int main() {
+
+  introduction("Beyonce", "Knowles");
+
+}
