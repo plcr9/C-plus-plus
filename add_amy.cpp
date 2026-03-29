5492ae127fe01977093161cec6f75f90d7f33eb2
Paul <118978940+plcr9@users.noreply.github.com>
2026-03-29 14:06:20 +0100

    Add main function to display song details

--- add_amy.cpp
+++ add_amy.cpp
@@ -1,1 +1,10 @@
+int main() {
+  
+  Song back_to_black("Back to Black", "Amy Winehouse");
 
+  std::cout << back_to_black.get_title() << 
+  "\n";
+  std::cout << back_to_black.get_artist() <<
+  "\n";
+
+}
