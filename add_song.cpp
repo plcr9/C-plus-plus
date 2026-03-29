785700265d3ca962a9f493e6e942ae3c2b53d533
Paul <118978940+plcr9@users.noreply.github.com>
2026-03-29 14:03:29 +0100

    Update add_song.cpp

--- add_song.cpp
+++ add_song.cpp
@@ -3,5 +3,7 @@ int main() {
   Song electric_relaxation;
   electric_relaxation.add_title("Electric Relaxation");
   std::cout << electric_relaxation.get_title():
+  electric_relaxation.add_artist("A Tribe Called Quest");
+  std::cout << electric_relaxation.get_artist();
 
 }
