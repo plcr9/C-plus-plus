871f2995b2a2d25c98a17772e275c9e2b05dd2c3
Paul <118978940+plcr9@users.noreply.github.com>
2026-03-29 14:13:17 +0100

    Implement Song class with title and artist
    Added a Song class with title and artist attributes, along with constructor, destructor, and getter methods.

--- add_amy_song.cpp
+++ add_amy_song.cpp
@@ -1,1 +1,43 @@
+int main () {
 
+  Song back_to_black("Back to Black", "Amy Winehouse");
+
+}
+
+class Song {
+
+  std::string title;
+  std::string artist;
+
+public:
+
+  Song(std::string new_title, std::string new_artist);
+
+  ~Song();
+
+  std::string get_title();
+
+  std::string get_artist();
+
+};
+
+Song:Song(std::string new_title, std::string new_artist)
+  : title(new_title), artist(new_artist) {}
+
+Song::~Song() {
+
+  std::cout << "Goodbye, " << title << "\n";
+
+}
+
+std::string Song::get_title() {
+
+  return title;
+
+}
+
+std::string Song::get_artist() {
+
+  return artist;
+
+}
