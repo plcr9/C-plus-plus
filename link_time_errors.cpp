d958ec16fa300e63dfcdb80a7d3d76bd9f2ec75b
Paul <118978940+plcr9@users.noreply.github.com>
2026-03-29 14:18:15 +0100

    Implement sum calculation and user input

--- link_time_errors.cpp
+++ link_time_errors.cpp
@@ -1,1 +1,18 @@
+int main() {
 
+  int num = 0;
+  int sum = 0;
+
+  std::cout << "Enter a number: ";
+  std::cin >> num;
+
+  for (int i = 1; i <= num; i++) {
+
+    sum = sum + i;
+    std::cout << i << " ";
+
+  }
+
+  std::cout << "Sum: " << sum << "\n";
+
+}
