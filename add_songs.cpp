760011e3ed72e69bc97f00313c9e3ffc88aa6b6b
Paul <118978940+plcr9@users.noreply.github.com>
2026-03-29 13:56:09 +0100

    Implement Song class with title management

--- add_songs.cpp
+++ add_songs.cpp
@@ -1,1 +1,17 @@
+int main() {
+  class Song {
+    std::string title;
 
+  public:
+    void add_title(std::string new_title);
+    std::string get_title();
+};
+
+void Song::add_title(std::string new_title) {
+    title = new_title;
+}
+
+std::string Song::get_title() {
+  return title;
+
+}
